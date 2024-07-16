cad86c139faa464fe486719ca519c8c72948bb94
ColdMeekly <20516479+ColdMeekly@users.noreply.github.com>
2024-07-16 22:52:18 +0100

    SMSDK Update

--- ports/smsdk/portfile.cmake
+++ ports/smsdk/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Scrap-Mods/SmSdk
     REF 79c38e542543fe5a4ee970b10a597a0254bab545
-    SHA512 0
+    SHA512 ab34129300fe09a15cfdf7ad58d27a726cabc5380f1ad3e7df5cca6d3233265373d5ceb2227affc1d88141564fa230d79fed7a5b1c795a18363ca0f05770f1ad
     HEAD_REF main
 )
 
